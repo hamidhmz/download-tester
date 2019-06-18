4c95d3a95544758851ff21ea3f70a1e9ac5e93e2
HamidReza Nasrollahi <nasrollahy.hamidreza@gmail.com>
2019-06-18 08:59:28 +0430

    fix problem minor changes

--- index.js
+++ index.js
@@ -6,24 +6,31 @@ const app = express();
 
 
 
-app.get("/",(req,res)=>{
+app.get("/:fileName/:time/:function",(req,res)=>{
     console.log("newUser");
-    const now = Math.floor(Date.now()/1000)
+    const now = Math.floor(Date.now()/1000);
+    console.log(req.params.fileName);
+    const filePath =  '/home/debian/test/file/'+ req.params.fileName;
+
+    switch(req.params.function)
+    {
+        case "stream":
+            streamDownloader.stream(filePath,res,now,req.params.time);//time In bytes per second
+            break;
     
-    const filePath =  '/home/debian/test/file/fs.apk';
-
-    // streamDownloader.stream(filePath,res,now,500);
-    streamDownloader.streamSleep(filePath,res,now,2);
-
-    // res.download(filePath, (err) => {
-    //     if (err) {
-    //         console.log(err);
-    //         return
-    //     }
-    //     console.log(Math.floor(Date.now()/1000) - now);
-    // });
+        case "streamSleep":
+            streamDownloader.streamSleep(filePath,res,now,req.params.time);//time is sleep time
+            break;
     
-
+        default:
+            res.download(filePath, (err) => {
+                if (err) {
+                    console.log(err);
+                    return
+                }
+                console.log(Math.floor(Date.now()/1000) - now);
+            });
+    }
 });
 
-app.listen(4040,()=>console.log("http://localhost/"));
+app.listen(4040,()=>console.log("http://localhost:4040/"));
